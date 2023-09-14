72621d254ef73920b93cf3cf1891c8f66fc0724f
Sumit <ss4366@njit.edu>
2023-09-14 16:25:29 -0400

    final commit prework

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -7,12 +7,14 @@ import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.helloPikachu)
         button.setOnClickListener{
-           Log.v("Hello world", "Button clicked!")
+           Log.v("Hello world!", "Button clicked!")
            Toast.makeText(this, "Hello Pikachu! Let's play", Toast.LENGTH_SHORT).show()
         }
     }
